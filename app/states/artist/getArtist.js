dc088a1f2d0cd2096d373aa1d14f79dc17323582
angelGarciaSantos <angel.garcia.90@gmail.com>
2017-05-10 21:09:17 +0200

    Mejorado funcionamiento boton seguir y dejar de seguir.

--- app/states/artist/getArtist.js
+++ app/states/artist/getArtist.js
@@ -16,16 +16,23 @@
 
                 vm.artistId = $stateParams.id;
                 vm.artist = artistsService.artists.get({ id: vm.artistId });
+				//vm.isFollowingArtist = artistsService.followArtist.get({ artistId: vm.artistId, userId: 1 });
+				
+				artistsService.followArtist.query({ artistId: vm.artistId, userId:1 }).$promise.then(function(data) {
+					vm.isFollowingArtist = data;
+				});	
 
 				ratingsService.globalRatingArtist.query({ id: vm.artistId }).$promise.then(function(data) {
 					vm.globalRating = data;
 				});
-
 				vm.follow = new artistsService.followArtist(); 
 
 				vm.followArtist = function () {
 					vm.follow.$save({ artistId: vm.artistId, userId: 1 }) //TODO: user id forzado
 						.then(function (result) {
+							artistsService.followArtist.query({ artistId: vm.artistId, userId:1 }).$promise.then(function(data) {
+								vm.isFollowingArtist = data;
+							});		
 							// cuando ha terminado el guardado del movimiento
 							// es momento de pedir una actualización de datos
 							//vm.nuevoMovimiento.importe = 0;
@@ -38,13 +45,17 @@
 				vm.unfollowArtist = function () {
 					vm.follow.$delete({ artistId: vm.artistId, userId: 1 })
 						.then(function (result) {
-
+							artistsService.followArtist.query({ artistId: vm.artistId, userId:1 }).$promise.then(function(data) {
+								vm.isFollowingArtist = data;
+							});	
 						}, function (error) {
 							console.error(error);
 						});
 				}
 
+				vm.$onInit = function() {
 
+				};
 			}
 		})
 
